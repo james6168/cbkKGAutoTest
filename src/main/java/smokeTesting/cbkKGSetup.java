31f7688d7e0d4b84ab893e420e2d49fa279a1f5b
james6168 <cingizkydraliev@gmail.com>
2022-01-09 17:09:58 +0600

    Небольшое обновление

--- src/main/java/smokeTesting/cbkKGSetup.java
+++ src/main/java/smokeTesting/cbkKGSetup.java
@@ -2,6 +2,7 @@ package smokeTesting;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -322,6 +323,4 @@ public class cbkKGSetup {
         objcbkKGibankPage = new cbkKGibankPage(driver);
         objcbkKGibankPage.cbkibankLanguagesLinkEnglishLinkSmokeTesting();
     }
-
-
 }
